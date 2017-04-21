b2e604fa5d9411d467efa9c8a0984eea9018804d
tttxxx52 <tttxxx52@gmail.com>
2017-04-21 14:31:24 +0800

    加入專案檔案。

---
+++ WorkShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorkShop.Startup))]
+namespace WorkShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
